407ae3ef8a6785f82c886c2e726bfb2d23aac3ff
andcoderweb <andersoncoderweb@gmail.com>
2019-11-30 17:20:35 -0300

    Criando Repository e Service

---
+++ src/main/java/com/andersonoliveira/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.andersonoliveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonoliveira.cursomc.domain.Categoria;
+import com.andersonoliveira.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}
